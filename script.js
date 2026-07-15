50a7e2355b81cb9502e37a9e5b5bda878f88d03d
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-16 00:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// const buttons = document.getElementById("btn");
-// const stpbutton = document.getElementById("stop");
+const buttons = document.getElementById("btn");
+const stpbutton = document.getElementById("stop");
 
-// let currentAudio = null;
+let currentAudio = null;
 
-// buttons.forEach((button)=>{
-// 	button.addEventListener("click", () =>{
-// 		if (currentAudio) {
-// 	      currentAudio.pause();
-// 	      currentAudio.currentTime = 0;
-// 	    }
+buttons.forEach((button)=>{
+	button.addEventListener("click", () =>{
+		if (currentAudio) {
+	      currentAudio.pause();
+	      currentAudio.currentTime = 0;
+	    }
 		
-//     const soundName = button.innerText.trim();
-//     currentAudio = new Audio(`sounds/${soundName}.mp3`);
-//     currentAudio.play();
-//   });
+    const soundName = button.innerText.trim();
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
 
-// });
+});
 
-// stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener("click", () => {
 
-//   if (currentAudio) {
-//     currentAudio.pause();
-//     currentAudio.currentTime = 0;
-//   }
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
 	
-// });
+});
