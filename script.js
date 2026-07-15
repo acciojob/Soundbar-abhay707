0602cafcc1d0ed647b7ee77ae5da90fc064a2fe5
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-16 00:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+// const buttons = document.getElementById("btn");
+// const stpbutton = document.getElementById("stop");
+
+// let currentAudio = null;
+
+// buttons.forEach((button)=>{
+// 	button.addEventListener("click", () =>{
+// 		if (currentAudio) {
+// 	      currentAudio.pause();
+// 	      currentAudio.currentTime = 0;
+// 	    }
+		
+//     const soundName = button.innerText.trim();
+//     currentAudio = new Audio(`sounds/${soundName}.mp3`);
+//     currentAudio.play();
+//   });
+
+// });
+
+// stopBtn.addEventListener("click", () => {
+
+//   if (currentAudio) {
+//     currentAudio.pause();
+//     currentAudio.currentTime = 0;
+//   }
+	
+// });
